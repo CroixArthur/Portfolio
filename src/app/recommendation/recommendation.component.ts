b9da7bd58fa382a2da969d9b131f2704ed482f9b
CroixArthur <arthur.croix.pro@gmail.com>
2024-07-09 16:06:29 +0200

    ✨ Reduced image size, updated links ux, updated home path to root

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -9,7 +9,7 @@ import { Recommendation } from "../models/recommendation.model";
   template: `
   <div class="dark:bg-gray-700 bg-gray-300 rounded-lg p-4">
     <div class="flex justify-between pb-2"><h3>{{recommendation.name}}</h3><img class="max-w-12 max-h-12 object-contain" [src]="recommendation.logoPath"/></div>
-    <a class="rounded-lg bg-white text-black flex justify-between items-center w-full p-4" [href]="recommendation.pdfPath" download><p>{{getFileName()}}</p><img class="w-8" src="assets/pdf-logo.png"/></a>
+    <a class="rounded-lg bg-white hover:bg-neutral-200 text-black flex justify-between items-center w-full p-4" [href]="recommendation.pdfPath" download><p>{{getFileName()}}</p><img class="w-8" src="assets/pdf-logo.png"/></a>
   </div>
   `
 })
